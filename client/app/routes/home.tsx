0df43843a6c600097d6f86ab6dbb86d6578bc9d5
Bartosz Trusiński <bartosztrusinski@gmail.com>
2026-02-12 22:45:26 +0100

    add biome linter and formatter

--- client/app/routes/home.tsx
+++ client/app/routes/home.tsx
@@ -1,15 +1,12 @@
-import type { Route } from './+types/home';
-
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: 'Chat App' },
-    {
-      name: 'description',
-      content: 'A real-time chat application built with React and Socket.IO.',
-    },
-  ];
-}
-
 export default function Home() {
-  return <div>Hello world</div>;
+	return (
+		<div>
+			<title>Chat App</title>
+			<meta
+				name="description"
+				content="A real-time chat application built with React and Socket.IO."
+			/>
+			<h1>Welcome to the Chat App</h1>
+		</div>
+	);
 }
--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
-import express from 'express';
 import cors from 'cors';
+import express from 'express';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello from the server');
+app.get('/', (_, res) => {
+	res.send('Hello from the server');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+	console.log(`Server is running on port ${PORT}`);
 });
