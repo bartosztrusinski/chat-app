30a007e8af3cb76d10aaef2cddf29a0fb9494dbc
Bartosz Trusiński <bartosztrusinski@gmail.com>
2026-02-19 19:43:23 +0100

    make util work with ssr

--- client/app/get-user-id.ts
+++ client/app/get-user-id.ts
@@ -1,6 +1,11 @@
 const USER_ID_KEY = 'user-id';
 
 export function getUserId() {
+	// fallback for SSR
+	if (typeof sessionStorage === 'undefined') {
+		return null;
+	}
+
 	const userId = sessionStorage.getItem(USER_ID_KEY);
 
 	if (!userId) {
