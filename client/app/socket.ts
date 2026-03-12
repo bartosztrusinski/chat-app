a7960afdd0582f9b267214f7a90936f1d5cca323
Bartosz Trusiński <bartosztrusinski@gmail.com>
2026-03-13 00:00:43 +0100

    expose server url env var to client

--- client/app/socket.ts
+++ client/app/socket.ts
@@ -2,7 +2,7 @@ import { io, type Socket } from 'socket.io-client';
 import { getUser } from '~/user';
 import type { ClientToServerEvents, ServerToClientEvents } from '../../types';
 
-const SERVER_URL = import.meta.env.SERVER_URL ?? 'http://localhost:5000';
+const SERVER_URL = import.meta.env.VITE_SERVER_URL ?? 'http://localhost:5000';
 
 export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(SERVER_URL, {
 	transports: ['websocket', 'polling', 'flashsocket'],
